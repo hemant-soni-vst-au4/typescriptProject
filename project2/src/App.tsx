ecd60d516b5341acf36ce04980c81b2d7efbea79
Hemant soni <sonihemant4741@gmail.com>
2023-01-20 12:26:50 +0530

    Setup input component

--- project2/src/App.tsx
+++ project2/src/App.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
+import Input from './Component/Input';
 
 const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("")
   return (
     <div className="App">
       <span className="heading">Task To DO</span>
+      <Input todo={todo} setTodo={setTodo} />
     </div>
   );
 }
 
 export default App;
+
+
+
