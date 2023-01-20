757e24e31b7219b3ebec0a98a96f6f9f6af2dd30
Hemant soni <sonihemant4741@gmail.com>
2023-01-20 16:38:48 +0530

    Drag and drop functionality

--- project2/src/Component/Input.tsx
+++ project2/src/Component/Input.tsx
@@ -1,22 +1,34 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
-interface Props{
+interface Props {
   todo: string;
   setTodo: React.Dispatch<React.SetStateAction<string>>;
   handleAdd: (e: React.FormEvent) => void;
 }
 
-const Input: React.FC<Props> = ({ todo, setTodo, handleAdd}) => {
-  return (
-    <form onSubmit={handleAdd} className="input">
-      <input type="input" placeholder="Enter a task" value={todo}
-      onChange={(e) => setTodo(e.target.value)}
-      className="input__box" />
-      
-      <button
-      className="input__submit">Go</button>
-    </form>
-  )
-}
+const Input: React.FC<Props> = ({ todo, setTodo, handleAdd }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
 
-export default Input
+  return (
+    <form
+      onSubmit={(e) => {
+        handleAdd(e);
+        inputRef.current?.blur();
+      }}
+      className="input"
+    >
+      <input
+        ref={inputRef}
+        type="input"
+        placeholder="Enter a task"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+        className="input__box"
+      />
+
+      <button className="input__submit">Go</button>
+    </form>
+  );
+};
+
+export default Input;
